463ac6e2ebeabbe81033c6c3ed26dea45cdec4ce
ephraimrosenfeld <ephraimrosenfeld@gmail.com>
2017-01-08 23:26:08 -0500

    Adding test method for RE
    New method is based upon Spring 2016 term project where both regex and tree-structure patterns were used for relation extraction

--- rel_extract/rel_extract.py
+++ rel_extract/rel_extract.py
@@ -12,6 +12,49 @@ class RelationalEntity(object):
     def __repr__(self):
         return repr('sub_type={0}, obj_type={1}, sub={2}, obj={3}'.format(self.subj_type, self.obj_type, self.subj, self.obj))
 
+
+def get_rel(tokens, tree, sub_precedes_obj):
+    if len(tokens) == 0 or len(tree) == 0 or (len(tree) < len(tokens)) :
+        return None
+    sub_type, obj_type, sub, obj = '', '', '', ''    
+    token_index = 0    
+    subj_found, obj_found    = False, False
+    for i in range((len(tree) - len(tokens))):
+        val = ''
+        print(str(i) + ': ' + str(type(tree[i])))
+        if isinstance(tree[i], nltk.tree.Tree):
+            print(str(tree[i]) + ' is a tree')
+            val = tree[i].label()
+        else :#tuple    
+            val = str(tree[i][1])
+        print('val: ' + val)    
+        if tokens[token_index].lower() == val.lower():
+            if isinstance(tree[i], nltk.tree.Tree):
+                if sub_precedes_obj:
+                    if subj_found == False :
+                        subj_found     = True
+                        sub_type     = tokens[i]
+                        sub            = ' '.join([x[0] for x in tree[i].leaves()])
+                    else:
+                        obj_found    = True
+                        obj_type     = tokens[i]
+                        obj            = ' '.join([x[0] for x in tree[i].leaves()])
+                else:
+                    if obj_found == False :
+                        obj_found     = True
+                        obj_type     = tokens[i]
+                        obj            = ' '.join([x[0] for x in tree[i].leaves()])
+                    else:
+                        subj_found     = True
+                        sub_type          = tokens[i]
+                        sub            = ' '.join([x[0] for x in tree[i].leaves()])
+            token_index = token_index + 1
+            if subj_found and obj_found: #match found
+                return RelationalEntity(sub_type, obj_type, sub, obj)
+        else:
+            continue
+    return None   
+
 def findrelations(text):
     entities = [];
     
@@ -26,7 +69,10 @@ def findrelations(text):
     sub                     = 'ORGANIZATION'
     obj                     = 'GPE'      
     for doc in chunked_sentences:
+        print(doc)
         for rel in nltk.sem.extract_rels(sub, obj, doc, corpus='ace', pattern=IN):
             raw_entity = nltk.sem.clause(rel, '')
+            print(raw_entity)
             entities.append(RelationalEntity(sub, obj, raw_entity[2:(raw_entity.find('\'',2))], raw_entity[(raw_entity.find('\'',2)+4):(len(raw_entity)-2)]))
+    print('entities retrieved: '+ str(len(entities)))
     return entities
